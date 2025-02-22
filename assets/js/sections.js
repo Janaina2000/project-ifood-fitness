9a8701649432892a8a6a21492deceb0aaed1fd0a
Janaina Front-end <janaina_2012silva@hotmail.com>
2025-02-22 19:28:06 -0300

    estruturando projeto

---
+++ assets/js/sections.js
@@ -0,0 +1,32 @@
+// Função para carregar o conteúdo de um arquivo HTML e inseri-lo em um elemento
+function loadHTML(file, selector) {
+  fetch(file)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Falha ao carregar ' + file);
+          }
+          return response.text();
+      })
+      .then(data => {
+          document.querySelector(selector).innerHTML = data;
+      })
+      .catch(error => console.error('Erro ao carregar o HTML:', error));
+}
+
+
+// Carregar o cabeçalho e o rodapé ao carregar o documento
+document.addEventListener('DOMContentLoaded', () => {
+  // Carregar HTML
+  loadHTML('src/html/pages/home/sections/01_hero.html', '#hero');
+  loadHTML('src/html/pages/home/sections/02_biography.html', '#biography');
+  loadHTML('src/html/pages/home/sections/03_experience.html', '#experience');
+  loadHTML('src/html/pages/home/sections/04_dreams.html', '#dreams');
+  loadHTML('src/html/pages/home/sections/05_skil.html', '#skil');
+  loadHTML('src/html/pages/home/sections/06_hobbis.html', '#hobbis');
+  loadHTML('src/html/pages/home/sections/07_projects.html', '#projects');
+
+  
+
+
+});
+
